49e018ab7e8d69540c6332d39eb9bdcebf65a855
Dana Brooks <dana.brooks@unify.com>
2014-11-05 22:25:08 -0500

    Clean up and fix timing of sound

--- WeBowl/GameScene.m
+++ WeBowl/GameScene.m
@@ -9,29 +9,32 @@
 // This is a test -- modified by M. Kelly
 
 #import "GameScene.h"
+#import "BowlScore.h"
 
+#if 0
 static const uint32_t ballCategory = 0x1 << 0;
 static const uint32_t pinCategory = 0x1 << 1;
+#endif
 
 @implementation GameScene {
     SKSpriteNode* ballSprite;
     SKSpriteNode* pinSprite;
     NSArray* pins;
+    BowlScore* bowlScore;
 }
 
 -(void)didMoveToView:(SKView *)view {
-    /* Setup your scene here */
-    SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+
     
     self.physicsWorld.contactDelegate = self;
-    // self.physicsWorld.gravity = CGPointVector(0, -1.0);
-    myLabel.text = @"Hello, World!";
-    myLabel.fontSize = 65;
-    myLabel.position = CGPointMake(CGRectGetMidX(self.frame),
-                                   CGRectGetMidY(self.frame));
-    myLabel = [self myScoreBoard];
-    [self addChild:myLabel];
+
+    bowlScore = [[BowlScore alloc] init];
     
+    // Add the scoreboard
+    SKLabelNode* scoreboardLabel = [self myScoreBoard];
+    [self addChild:scoreboardLabel];
+    
+    // Setup the background image of the bowling lane
     SKSpriteNode *bgImage = [SKSpriteNode spriteNodeWithImageNamed:@"lane.jpeg"];
     bgImage.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     bgImage.yScale = 3.0;
@@ -39,15 +42,13 @@ static const uint32_t pinCategory = 0x1 << 1;
     [self addChild:bgImage];
     
     
-    ballSprite = [SKSpriteNode spriteNodeWithImageNamed:@"bowlingball2"];
-    ballSprite.xScale = 0.5;
-    ballSprite.yScale = 0.5;
-    
     // Add the ball
-    ballSprite.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) - 220);
-    NSLog(@"x = %f", [[UIScreen mainScreen] bounds].size.width / 2);
-    NSLog(@"bounds = %@", NSStringFromCGRect([[UIScreen mainScreen] bounds]));
-
+//    ballSprite = [SKSpriteNode spriteNodeWithImageNamed:@"bowlingball2"];
+//    ballSprite.xScale = 0.5;
+//    ballSprite.yScale = 0.5;
+//    ballSprite.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) - 220);
+    [self initBall];
+    
 #if 0
     // Physics
     ballSprite.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:(ballSprite.size.width/2)-7];
@@ -58,17 +59,12 @@ static const uint32_t pinCategory = 0x1 << 1;
     ballSprite.physicsBody.contactTestBitMask = ballCategory | pinCategory;
 #endif
     
-    [self addChild:ballSprite];
+    // [self addChild:ballSprite];
 
     pins = [self createPins];
     for (SKSpriteNode* s in pins) {
         [self addChild:s];
     }
-    //    pinSprite = [SKSpriteNode spriteNodeWithImageNamed:@"pin.jpeg"];
-//    pinSprite.xScale = 0.2;
-//    pinSprite.yScale = 0.2;
-//    pinSprite.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame) + 200);
-//    [self addChild:pinSprite];
     [self addChild: [self resetButtonNode]];
 }
 
@@ -101,46 +97,6 @@ static const uint32_t pinCategory = 0x1 << 1;
     return pinArray;
 }
 
-//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    /* Called when a touch begins */
-//    
-//    for (UITouch *touch in touches) {
-//        CGPoint location = [touch locationInNode:self];
-//        
-//        SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-//        
-//        sprite.xScale = 0.5;
-//        sprite.yScale = 0.5;
-//        sprite.position = location;
-//        
-//        SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-//        
-//        [sprite runAction:[SKAction repeatActionForever:action]];
-//        
-//        [self addChild:sprite];
-//    }
-//}
-
-//-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-//    
-//    for (UITouch *touch in touches) {
-//        
-//        for (UITouch *touch in touches) {
-//            
-//            CGPoint location = [touch locationInNode:self];
-//            
-//            CGPoint diff = CGPointMake(location.x - self.hero.position.x, location.y - self.hero.position.y);
-//            
-//            CGFloat angleRadians = atan2f(diff.y, diff.x);
-//            
-//            [self.hero runAction:[SKAction sequence:@[
-//                                                      [SKAction rotateToAngle:angleRadians duration:1.0],
-//                                                      [SKAction moveByX:diff.x y:diff.y duration:3.0]
-//                                                      ]]];
-//        }
-//    }
-//}
-
 - (SKLabelNode *)myScoreBoard
 {
     SKLabelNode *myLabel = [SKLabelNode labelNodeWithFontNamed:@"Arial"];
@@ -167,6 +123,7 @@ static const uint32_t pinCategory = 0x1 << 1;
         return;
     }
     
+#if 0
     // [self runAction:[SKAction playSoundFileNamed:@"strike.wav" waitForCompletion:NO]];
     [self runAction:[SKAction playSoundFileNamed:@"strike.wav" waitForCompletion:YES] completion:^(void) {
         NSLog(@"After the ball struck the pins");
@@ -177,15 +134,27 @@ static const uint32_t pinCategory = 0x1 << 1;
         [pins[2] removeFromParent];
         
     }];
-    // CGPoint location = [touch locationInNode:self];
+#endif
+
+    // Play the sound
+    [self runAction:[SKAction playSoundFileNamed:@"strike.wav" waitForCompletion:NO]];
+
+    // Calculate where the ball should end up
     CGPoint diff = CGPointMake(ballSprite.position.x - 20, ballSprite.position.y + 250);
     CGFloat angleRadians = atan2f(diff.y, diff.x);
     [ballSprite runAction:[SKAction group:@[
                                             [SKAction rotateToAngle:angleRadians duration:3.0],
-                                            //                                                      [SKAction moveByX:diff.x y:diff.y duration:4.0],
                                             [SKAction moveTo:diff duration:3.0],
                                             [SKAction scaleBy:0.2 duration:3.0]
-                                            ]]];
+                                            ]] completion:^(void) {
+            [ballSprite removeFromParent];
+            [pins[9] removeFromParent];
+            [pins[3] removeFromParent];
+            [pins[6] removeFromParent];
+            [pins[2] removeFromParent];
+    }];
+    [self getPinsKnockedDown];
+    NSLog(@"Done moving");
     
     
 }
@@ -241,6 +210,23 @@ static const uint32_t pinCategory = 0x1 << 1;
         [self addChild:s];
     }
 
+}
+
+-(NSArray*)getPinsKnockedDown {
+    NSArray* p = [[NSArray alloc] init];
+
+    int numberKnockedDown = (arc4random() % 10) + 1;
+    NSLog(@"numberKnockedDown = %d", numberKnockedDown);
+    if (numberKnockedDown != 10) {
+    }
+    else {
+        int counter = 0;
+        while (counter < numberKnockedDown) {
+            // int pinKnockedDown = (arc4random() % 10) + 1;
+        }
+    }
+    
+    return p;
 }
 
 @end
